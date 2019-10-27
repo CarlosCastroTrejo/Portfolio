d6e54c323e97533f94b60e1dfeb8b39358ad3b1f
Carlos Emiliano Castro Trejo <A01422062@itesm.mx>
2019-10-27 17:52:02 -0600

    Almost every is ready in the webpage. Libraries were added to the vendor folder.

--- js/resume.js
+++ js/resume.js
@@ -1,8 +1,10 @@
-(function($) {
+$(document).ready(() => {
   "use strict"; // Start of use strict
 
+  $('[data-toggle="popover"]').popover();
+
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +18,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -24,5 +26,4 @@
   $('body').scrollspy({
     target: '#sideNav'
   });
-
-})(jQuery); // End of use strict
+});
